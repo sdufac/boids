435a9ae8e520f24d2305c7a6dfc5acf13ba80be9
sdufac <simon.dufac@gmail.com>
2024-07-09 17:51:56 +0200

    main without definition

--- main.c
+++ main.c
@@ -15,22 +15,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "rlgl.h"
-
-typedef struct{
-        Vector2 position;
-        Vector2 velocity;
-        float rotation;
-        float protectedRange;
-        float visualRange;
-        float maxSpeed;
-        float minSpeed;
-} Boid;
-
-void speedCap(Boid * boid);
-void GetSeparation(Boid * boid,Boid boids[],int nbBoid);
-void BoidAlign(Boid * boid, Boid boids[], int nbBoid);
-void avoidBorder(Boid * boid);
-void boidCohesion(Boid * boid, Boid boids[],int nbBoid);
+#include "boid.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -73,19 +58,16 @@ int main(void)
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
         for(int i=0;i<nbBoid;i++){
-            //Separate boids
-            GetSeparation(&boids[i],boids,nbBoid);
-            //Matching Velocity between boids
-            BoidAlign(&boids[i],boids,nbBoid);
+            getSeparation(&boids[i],boids,nbBoid);
+            boidAlign(&boids[i],boids,nbBoid);
             boidCohesion(&boids[i],boids,nbBoid);
-            //Avoid screen border
             avoidBorder(&boids[i]);
-            //Cap the speed of boid
             speedCap(&boids[i]);
-            //Move the boid according to velocity
+
             float delta = GetFrameTime();
             boids[i].position = Vector2Add(boids[i].position,Vector2Scale(boids[i].velocity,delta));
         }
+
         //----------------------------------------------------------------------------------
         BeginDrawing();
             ClearBackground(RAYWHITE);
@@ -108,88 +90,4 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
-}
-
-void speedCap(Boid * boid){
-    
-    //Speed Checker
-    if(Vector2Length(boid->velocity)<boid->minSpeed){
-        boid->velocity = Vector2Scale(Vector2Normalize(boid->velocity),boid->minSpeed);
-    }
-    if(Vector2Length(boid->velocity)>boid->maxSpeed){
-        boid->velocity = Vector2Scale(Vector2Normalize(boid->velocity),boid->maxSpeed);
-    }
-    
-}
-
-//Check separation factor with other boids and with screen border
-void GetSeparation(Boid * boid,Boid boids[],int nbBoid){
-    Vector2 close = {0,0};
-
-    for(int i = 0; i < nbBoid; i++){
-        if(CheckCollisionCircles(boid->position,boid->protectedRange,boids[i].position,2)){
-            close = Vector2Subtract(boid->position,boids[i].position);
-        }
-    } 
-
-    boid->velocity = Vector2Add(boid->velocity,Vector2Scale(close,0.3)); 
-}
-
-//Check alignement with other boids
-void BoidAlign(Boid * boid, Boid boids[], int nbBoid){
-    int boidCount = 0;
-    Vector2 velAvg = {0,0};
-
-    for(int i = 0 ; i<nbBoid; i++){
-        if(CheckCollisionCircles(boid->position,boid->visualRange,boids[i].position,1) && !CheckCollisionCircles(boid->position,boid->protectedRange,boid[i].position,1)){
-            boidCount++;
-            velAvg = Vector2Add(velAvg, boids[i].velocity);
-        }
-    }
-
-    if(boidCount > 0){
-        velAvg.x = velAvg.x / boidCount;
-        velAvg.y = velAvg.y / boidCount;
-    }
-    
-    boid->velocity = Vector2Add(boid->velocity,Vector2Scale(Vector2Subtract(velAvg,boid->velocity),0.1)); //2 = matching factor
-}
-
-void avoidBorder(Boid * boid){
-    //Screen border
-    int turnFactor = 10;
-    if(boid->position.x < 100){
-        boid->velocity.x = boid->velocity.x + turnFactor;
-    }
-    else if(boid->position.x > 900){
-        boid->velocity.x = boid->velocity.x - turnFactor;
-    }
-    
-    if(boid->position.y < 50){
-        boid->velocity.y = boid->velocity.y + turnFactor;
-    }
-    else if(boid->position.y > 650){
-        boid->velocity.y = boid->velocity.y - turnFactor;
-    }
-}
-
-void boidCohesion(Boid * boid, Boid boids[],int nbBoid){
-    float centeringFactor = 0.005;
-
-    Vector2 posAvg = {0,0};
-    int boidCount=0;
-
-    for(int i = 0 ; i<nbBoid; i++){
-        if(CheckCollisionCircles(boid->position,boid->visualRange,boids[i].position,1) && !CheckCollisionCircles(boid->position,boid->protectedRange,boid[i].position,1)){
-            boidCount++;
-            posAvg = Vector2Add(posAvg,boids[i].position);
-        }
-    }
-
-    if(boidCount > 0){
-        posAvg.x = posAvg.x/boidCount;
-        posAvg.y = posAvg.y/boidCount;
-    }
-
-    boid->velocity = Vector2Add(boid->velocity,Vector2Scale(Vector2Subtract(posAvg,boid->position),centeringFactor));
 }
